c4fcecfc02c6b45c866a63f2ed2313265c2ddaf6
Darkindast <109829419+Darkindast@users.noreply.github.com>
2022-09-07 19:00:40 +0300

    Update си.c

--- си.c
+++ си.c
@@ -10,13 +10,13 @@ int palindrom(int n){
 }
 
 int main(){
-    int ch, n, i = 0,f,r;
-    scanf("%d %d", &ch, &n);
-    for (;i < n; ++i){
+    int ch,f,r;
+    scanf("%d", &ch);
+    while (ch != palindrom(ch)){
         f = palindrom(ch);
         r = palindrom(f);
         ch = f + r;
     }
-    printf("%d", ch);
+    printf("%d\n", ch);
     return 0;
 }
